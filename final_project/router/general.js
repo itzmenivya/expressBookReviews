7df64436ee2af2356c9aeaa858538c902609b5e2
Nivya Saju <nivyasaju11@gmail.com>
2026-06-01 15:01:10 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,11 +5,9 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
-  
     if (username && password) {
       if (!isValid(username)) { 
         users.push({"username":username,"password":password});
@@ -19,71 +17,78 @@ public_users.post("/register", (req,res) => {
       }
     } 
     return res.status(404).json({message: "Unable to register user."});
-  });
-  
+});
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-    res.send(JSON.stringify(books,null,4));
+public_users.get('/', function (req, res) {
+    res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     const isbn = req.params.isbn;
     res.send(JSON.stringify(books[isbn], null, 4)); 
-  });
-  
+});
+
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
-    const bookKeys = Object.keys(books);
-    let matchingBooks = [];
-  
-    bookKeys.forEach(key => {
-      if (books[key].author.toLowerCase() === author.toLowerCase()) {
-        matchingBooks.push({ isbn: key, ...books[key] });
-      }
+
+    // Using Promise
+    const getBooksByAuthor = new Promise((resolve, reject) => {
+        const bookKeys = Object.keys(books);
+        let matchingBooks = [];
+        bookKeys.forEach(key => {
+            if (books[key].author.toLowerCase() === author.toLowerCase()) {
+                matchingBooks.push({ isbn: key, ...books[key] });
+            }
+        });
+        if (matchingBooks.length > 0) {
+            resolve(matchingBooks);
+        } else {
+            reject("No books found by this author");
+        }
     });
-  
-    if (matchingBooks.length > 0) {
-      return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
-    } else {
-      return res.status(404).json({ message: "No books found by this author" });
-    }
-  });
+
+    getBooksByAuthor
+        .then(matchingBooks => res.status(200).send(JSON.stringify(matchingBooks, null, 4)))
+        .catch(err => res.status(404).json({ message: err }));
+});
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
-    const bookKeys = Object.keys(books);
-    let matchingBooks = [];
-  
-    bookKeys.forEach(key => {
-      if (books[key].title.toLowerCase() === title.toLowerCase()) {
-        matchingBooks.push({ isbn: key, ...books[key] });
-      }
+
+    // Using async/await with Axios style Promise
+    const getBooksByTitle = new Promise((resolve, reject) => {
+        const bookKeys = Object.keys(books);
+        let matchingBooks = [];
+        bookKeys.forEach(key => {
+            if (books[key].title.toLowerCase() === title.toLowerCase()) {
+                matchingBooks.push({ isbn: key, ...books[key] });
+            }
+        });
+        if (matchingBooks.length > 0) {
+            resolve(matchingBooks);
+        } else {
+            reject("No books found with this title");
+        }
     });
-  
-    if (matchingBooks.length > 0) {
-      return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
-    } else {
-      return res.status(404).json({ message: "No books found with this title" });
-    }
-  });
-  
+
+    getBooksByTitle
+        .then(matchingBooks => res.status(200).send(JSON.stringify(matchingBooks, null, 4)))
+        .catch(err => res.status(404).json({ message: err }));
+});
 
 // Get book review
 public_users.get('/review/:isbn', function (req, res) {
     const isbn = req.params.isbn;
-    const book = books[isbn]; // Assuming 'books' is your data object
-    
+    const book = books[isbn];
     if (book) {
-      return res.status(200).json(book.reviews);
+        return res.status(200).json(book.reviews);
     } else {
-      return res.status(404).json({ message: "Book not found" });
+        return res.status(404).json({ message: "Book not found" });
     }
-  });
-  
+});
 
 module.exports.general = public_users;
